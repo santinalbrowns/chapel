3d2a59c7c9f8f7bbeca19678f8813cbb624eceef
Santinal Browns <asantikuseka@outlook.com>
2023-07-03 21:56:52 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	async function greet() {
+		await invoke('open_docs');
+	}
+</script>
+
+<button on:click="{greet}">Live</button>
+<button>Stop</button>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
